d779d8d828fc1c3a49926efa3dd0cbfbdd2d7072
Paul <pkilgarriff@gmail.com>
2022-02-14 12:59:10 +0000

    Passes all tests and add testing command

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -7,8 +7,10 @@
   # If so return "FizzBuzz"
 # If none of the above are true, return the number itself
 
+# Test with (1..100).to_a.each { |number| puts fizzbuzz(number) }
+
 def fizzbuzz(number)
-  return 'fizzbuzz' if number == 15
+  return 'fizzbuzz' if number % 15 == 0
   return 'fizz' if number % 3 == 0
   return 'buzz' if number % 5 == 0
   return number
