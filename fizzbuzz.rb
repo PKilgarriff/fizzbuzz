561f09fa5b9fb8511d43af2a78b735328ce1ee1e
Paul <pkilgarriff@gmail.com>
2022-02-14 11:47:20 +0000

    Add fizzbuzz specification

---
+++ fizzbuzz.rb
@@ -0,0 +1,8 @@
+# Get a number from the user
+# Check if it is a multiple of 3
+  # If so return "Fizz"
+# Check if it is a multiple of 5
+  # If so return "Buzz"
+# Check if it is a multiple of both 3 and 5
+  # If so return "FizzBuzz"
+# If none of the above are true, return the number itself
